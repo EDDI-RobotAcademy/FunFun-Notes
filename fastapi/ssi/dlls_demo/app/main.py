4732e73ce8acafd56b40adc4c53a4817827d588d
ajeseung <ajeseung@naver.com>
2024-12-24 15:09:13 +0900

    feat:[손승일] kmeans 구현

--- fastapi/ssi/dlls_demo/app/main.py
+++ fastapi/ssi/dlls_demo/app/main.py
@@ -6,6 +6,7 @@ import os
 from config.cors_config import CorsConfig
 from ensemble_method.controller.ensemble_method_controller import ensembleMethodRouter
 from feature_engineering.controller.feature_engineering_controller import featureEngineeringRouter
+from kmeans.controller.kmeans_controller import kMeansRouter
 
 load_dotenv()
 
@@ -15,7 +16,7 @@ CorsConfig.middlewareConfig(app)
 #APIRouter로 작성한 Router를 
 app.include_router(featureEngineeringRouter)
 app.include_router(ensembleMethodRouter)
-
+app.include_router(kMeansRouter)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv('FASTAPI_PORT')))
