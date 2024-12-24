ae9f226c70353c1a732f3d6d28817d7e5286a39f
Sanghoon Lee <gcccompil3r@gmail.com>
2024-12-24 17:52:22 +0900

    feat: [이상훈] 카트 추가 [SKAI8-x]

--- nuxt/notes/ui/cart/stores/cartActions.ts
+++ nuxt/notes/ui/cart/stores/cartActions.ts
@@ -1,4 +1,5 @@
 import * as axiosUtility from "../../utility/axiosInstance"
+import { Cart } from "./cartType"
 
 export const cartAction = {
     async requestCartList(page: number = 1, perPage: number = 10): Promise<void> {
@@ -17,4 +18,27 @@ export const cartAction = {
             console.log('requestGameSoftwareList() 중 에러:', error)
         }
     },
+    async requestCartSave(requestForm: { id: number, userToken: string, quantity: number }) {
+        const { djangoAxiosInstance } = axiosUtility.createAxiosInstances()
+
+        const cart = {
+            id: requestForm.id,
+            quantity: requestForm.quantity,
+        };
+
+        try {
+            const response = await djangoAxiosInstance.post("/cart/create", {
+                cart: cart, userToken: requestForm.userToken
+            });
+            console.log("서버에 장바구니 저장 성공:", response.data.success);
+            return response.data.success
+        } catch (error) {
+            console.error("서버에 장바구니 저장 실패:", error);
+            this.cartList = this.cartList.filter(
+                (item) => item.cartId !== cart.id
+            );
+
+            throw error;
+        }
+    }
 }
