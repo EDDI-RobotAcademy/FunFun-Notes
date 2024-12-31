7d19a28fe06b7ff3d2ba5af1594ec88f615b4c31
Hyeonseo2083 <122842851+jungs0914@users.noreply.github.com>
2024-12-31 11:08:47 +0900

    feat:[정현서] cart [SKAI8-x]

--- django/hs/db_automation/db_automation/urls.py
+++ django/hs/db_automation/db_automation/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("game-software/", include('game_software.urls')),
     path("regression/", include('regression.urls')),
     path("authentication/", include('authentication.urls')),
+    path("cart/", include('cart.urls')),
 ]
