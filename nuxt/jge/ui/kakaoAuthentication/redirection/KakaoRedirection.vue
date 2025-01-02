5398815e70d7055b68cf43a984146f78844c4a1e
GOEUN JU <133327235+Goeun-Ju@users.noreply.github.com>
2025-01-02 15:14:34 +0900

    feat: [주고은] 파일 복구 [SKAI8-x]

--- nuxt/jge/ui/kakaoAuthentication/redirection/KakaoRedirection.vue
+++ nuxt/jge/ui/kakaoAuthentication/redirection/KakaoRedirection.vue
@@ -18,16 +18,13 @@ const route = useRoute()
 const setRedirectKakaoData = async() => {
     const code = route.query.code
     const userToken = await kakaoAuthenticationStore.requestAccessToken({ code });
+
     localStorage.setItem("userToken", userToken)
     kakaoAuthenticationStore.isAuthenticated = true
+
     router.push('/')
-    // const isEmailDuplication = await kakaoAuthenticationStore.requestKakaoLoginToDjango({ email })
-    // if (isEmailDuplication === true) {
-    //     console.log('이미 가입한 회원입니다!')
-    // } else {
-    //     console.log('새로 가입이 필요한 회원입니다.')
-    // }
 }
+
 onMounted(async () => {
     await setRedirectKakaoData()
 })
