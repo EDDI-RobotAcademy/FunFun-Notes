02cb0802ffe10360abf0d80d70710b5fa3326479
ajeseung <ajeseung@naver.com>
2025-01-08 17:13:29 +0900

    feat:[손승일] 게시판 추가

--- django/ssi/db_automation/payments/entity/payments.py
+++ django/ssi/db_automation/payments/entity/payments.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from account.entity.account import Account
+from orders.entity.orders import Orders
 
 
 class Payments(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='payments')  # 외래키로 Account 모델 연결
     payment_key = models.CharField(max_length=255)  # 결제 키
     order_id = models.CharField(max_length=255)  # 주문 ID
+    orders_info = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name='payments')  # 외래키로 Orders 모델 연결
     amount = models.DecimalField(max_digits=10, decimal_places=2)  # 결제 금액
     provider = models.CharField(max_length=100)  # 결제 제공자
     method = models.CharField(max_length=100)  # 결제 방법
