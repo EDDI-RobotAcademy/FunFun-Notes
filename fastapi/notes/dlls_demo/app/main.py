1ddfe26a4dcf501ba701c33d0bb622e746dbdcd1
Sanghoon Lee <gcccompil3r@gmail.com>
2025-01-09 10:02:14 +0900

    fix: [이상훈] CNN 구동 시 tensorflow 오류 수정 [SKAI8-x]

--- fastapi/notes/dlls_demo/app/main.py
+++ fastapi/notes/dlls_demo/app/main.py
@@ -4,6 +4,7 @@ import uvicorn
 import os
 
 from config.cors_config import CorsConfig
+from convolution_neural_network.controller.cnn_controller import convolutionNeuralNetworkRouter
 from ensemble_method.controller.ensemble_method_controller import ensembleMethodRouter
 from feature_engineering.controller.feature_engineering_controller import featureEngineeringRouter
 from gradient_descent.controller.gradient_descent_controller import gradientDescentRouter
@@ -29,6 +30,7 @@ app.include_router(modelRegulationRouter)
 app.include_router(gradientDescentRouter)
 app.include_router(hyperParameterRouter)
 app.include_router(principalComponentAnalysisRouter)
+app.include_router(convolutionNeuralNetworkRouter)
 
 # HOST는 모두에 열려 있고
 # FASTAPI_PORT를 통해서 이 서비스가 구동되는 포트 번호를 지정
