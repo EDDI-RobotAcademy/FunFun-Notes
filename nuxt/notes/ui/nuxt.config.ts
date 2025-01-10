70bed88d5cf44f8f300f82837bcc28abfc49712c
Sanghoon Lee <gcccompil3r@gmail.com>
2025-01-10 12:03:10 +0900

    feat: [이상훈] 분석 요청 결과 수신 완료 [SKAI8-x]

--- nuxt/notes/ui/nuxt.config.ts
+++ nuxt/notes/ui/nuxt.config.ts
@@ -22,7 +22,10 @@ export default defineNuxtConfig({
   ],
 
   build: {
-    transpile: ['vuetify']
+    transpile: [
+      'vuetify',
+      'vue-toastification'
+    ]
   },
 
   vite: {
@@ -39,7 +42,7 @@ export default defineNuxtConfig({
 
   modules: [
     'vuetify-nuxt-module',
-    '@pinia/nuxt',
+    '@pinia/nuxt', 
     '~/home/index.ts',
     '~/pandas_basic/index.ts',
     '~/account/index.ts',
