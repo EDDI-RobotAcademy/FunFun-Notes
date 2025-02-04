0ab04f48d860ad2ba0de10aab63bc1bcb49ad7c1
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-04 10:12:13 +0900

    feat: [이상훈] Kakao Request User Info [SKAI8-x]

--- flutter/notes/first/lib/kakao_authentication/presentation/providers/kakao_auth_providers.dart
+++ flutter/notes/first/lib/kakao_authentication/presentation/providers/kakao_auth_providers.dart
@@ -2,8 +2,11 @@ import 'package:first/kakao_authentication/domain/usecase/login_usecase.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../../domain/usecase/fetch_user_info_usecase.dart';
+
 class KakaoAuthProvider with ChangeNotifier {
   final LoginUseCase loginUseCase;
+  final FetchUserInfoUseCase fetchUserInfoUseCase;
 
   final FlutterSecureStorage secureStorage = FlutterSecureStorage();
 
@@ -18,7 +21,8 @@ class KakaoAuthProvider with ChangeNotifier {
   String get message => _message;
 
   KakaoAuthProvider({
-    required this.loginUseCase
+    required this.loginUseCase,
+    required this.fetchUserInfoUseCase,
   });
 
   Future<void> login() async {
@@ -30,6 +34,14 @@ class KakaoAuthProvider with ChangeNotifier {
       print("Kakao loginUseCase.execute()");
       _accessToken = await loginUseCase.execute();
       print("AccessToken obtained: $_accessToken");
+
+      final userInfo = await fetchUserInfoUseCase.execute();
+      print("User Info fetched: $userInfo");
+
+      final email = userInfo.kakaoAccount?.email;
+      final nickname = userInfo.kakaoAccount?.profile?.nickname;
+
+      print("User email: $email, User nickname: $nickname");
     } catch (e) {
       _isLoggedIn = false;
       _message = "로그인 실패: $e";
