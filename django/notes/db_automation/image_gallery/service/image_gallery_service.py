9fa5a75b7fce8509c37d0aaab291451f128fcfea
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-05 15:17:52 +0900

    feat: [이상훈] Nuxt with AWS S3 [SKAI8-x]

--- django/notes/db_automation/image_gallery/service/image_gallery_service.py
+++ django/notes/db_automation/image_gallery/service/image_gallery_service.py
@@ -6,3 +6,7 @@ class ImageGalleryService(ABC):
     @abstractmethod
     def requestList(self, page, perPage):
         pass
+
+    @abstractmethod
+    def requestCreate(self, title, image_url):
+        pass
