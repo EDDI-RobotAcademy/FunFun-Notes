ad37684c623b9cdbd14551ba18d7734fe45f226f
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-05 12:13:19 +0900

    feat: [이상훈] LLM과 대화하기 [SKAI8-x]

--- flutter/notes/first/lib/simple_chat/presentation/ui/simple_chat_page.dart
+++ flutter/notes/first/lib/simple_chat/presentation/ui/simple_chat_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/simple_chat_provider.dart';
+
 class SimpleChatPage extends StatefulWidget {
 
   @override
--- flutter/notes/first/lib/simple_chat/simple_chat_module.dart
+++ flutter/notes/first/lib/simple_chat/simple_chat_module.dart
@@ -1,35 +1,42 @@
-import 'package:first/kakao_authentication/presentation/providers/kakao_auth_providers.dart';
-import 'package:first/kakao_authentication/presentation/ui/kakao_login_page.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'infrastructures/data_sources/simple_chat_remote_data_source.dart';
+import 'infrastructures/repository/simple_chat_repository_impl.dart';
+import 'presentation/providers/simple_chat_provider.dart';
+import 'domain/usecases/send_simple_chat_usecase_impl.dart';
+import 'presentation/ui/simple_chat_page.dart';
 
 class SimpleChatModule {
-  static Widget provideSimpleChatPage() {
+  static Widget provideSimpleChatPage(String apiUrl, String apiKey) {
+    print("provideSimpleChatPage apiUrl: ${apiUrl}");
+    print("provideSimpleChatPage apiKey: ${apiKey}");
+
     return MultiProvider(
       providers: [
         Provider<SimpleChatRemoteDataSource>(
-          create: (_) => SimpleChatRemoteDataSource()
+          create: (_) =>
+              SimpleChatRemoteDataSource(apiUrl: apiUrl, apiKey: apiKey),
         ),
-        ProxyProvider<SimpleChatRemoteDataSource, SimpleChatRepository>(
+        ProxyProvider<SimpleChatRemoteDataSource, SimpleChatRepositoryImpl>(
           update: (_, remoteDataSource, __) =>
-            SimpleChatRepositoryImpl(remoteDataSource),
+              SimpleChatRepositoryImpl(remoteDataSource),
         ),
-        ProxyProvider<SimpleChatRepository, SendSimpleChatUseCaseImpl>(
-          update: (_, repository, __) =>
-            SendSimpleChatUseCaseImpl(repository)
+        ProxyProvider<SimpleChatRepositoryImpl, SendSimpleChatUseCaseImpl>(
+          update: (_, repository, __) => SendSimpleChatUseCaseImpl(repository),
         ),
-        ChangeNotifierProvider<SendSimpleChatUseCaseImpl, SimpleChatProvider>(
+        ChangeNotifierProxyProvider<SendSimpleChatUseCaseImpl,
+            SimpleChatProvider>(
           create: (context) => SimpleChatProvider(
             sendSimpleChatUseCase: context.read<SendSimpleChatUseCaseImpl>(),
           ),
           update: (_, useCase, previousProvider) {
             previousProvider?.updateUseCase(useCase);
-            return previousProvider ?? SimpleChatProvider(sendSimpleChatUseCase: useCase);
-          }
+            return previousProvider ??
+                SimpleChatProvider(sendSimpleChatUseCase: useCase);
+          },
         ),
       ],
-      child: SimpleChatPage()
+      child: SimpleChatPage(),
     );
   }
 }
