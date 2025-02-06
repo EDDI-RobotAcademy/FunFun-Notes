87ffb9cbbe2c1768f0aa2736a3b4ec94ecf8242e
GOEUN JU <133327235+Goeun-Ju@users.noreply.github.com>
2025-02-06 17:52:14 +0900

    feat: [주고은] 보드 및 로그인 수정 [SKAI8-x]

--- django/jge/db_automation/orders/urls.py
+++ django/jge/db_automation/orders/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
+from orders.controller.order_controller import OrderController
 
 router = DefaultRouter()
-router.register(r"order", OrderController, basename='order')
+router.register(r"orders", OrderController, basename='orders')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('create',
          OrderController.as_view({ 'post': 'requestCreateOrder' }),
-         name='주문 정보 생성'),
-    # path('list',
-    #      CartController.as_view({ 'post': 'requestListOrder' }),
-    #      name='주문 리스트'),
+         name='주문 생성'),
 ]
