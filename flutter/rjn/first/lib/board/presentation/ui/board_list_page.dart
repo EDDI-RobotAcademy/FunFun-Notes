8ad5b7b876f095e0bbc554048e19ea3f56f56b6b
제나 <denalog@naver.com>
2025-02-10 11:18:55 +0900

    feat :[유제나] flutter login / board list 구현 [SKAI8-x]

--- flutter/rjn/first/lib/board/presentation/ui/board_list_page.dart
+++ flutter/rjn/first/lib/board/presentation/ui/board_list_page.dart
@@ -1,9 +1,13 @@
+import 'package:first/board/board_module.dart';
 import 'package:first/common_ui/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../common_ui/error_message.dart';
+import '../../../common_ui/loading_indicator.dart';
 import '../providers/board_list_provider.dart';
+import 'component/page_content.dart';
 
 class BoardListPage extends StatefulWidget {
   @override
@@ -42,6 +46,13 @@ class _BoardListPageState extends State<BoardListPage> {
 
   @override
   Widget build(BuildContext context) {
+    // AppBar 기본 높이
+    final double appBarHeight = kToolbarHeight;
+    // 상태 표시줄
+    final double statusBarHeight =
+        MediaQuery.of(context).padding.top;
+    // 간격 계산 시 활용
+    final double contentTopPadding = appBarHeight;
     
     return Scaffold(
       appBar: PreferredSize(
@@ -51,6 +62,57 @@ class _BoardListPageState extends State<BoardListPage> {
           title: '게시판',
         ),
       ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: contentTopPadding),
+              child: Consumer<BoardListProvider>(
+                builder: (context, boardListProvider, child) {
+                  if (boardListProvider.isLoading &&
+                      boardListProvider.boardList.isEmpty) {
+                    // 아직 준비 중일 때
+                    return LoadingIndicator();
+                  }
+
+                  if (boardListProvider.message.isNotEmpty) {
+                    return ErrorMessage(
+                      message: boardListProvider.message
+                    );
+                  }
+
+                  return PageContent(
+                    boardListProvider: boardListProvider,
+                    onPageChanged: onPageChanged,
+                  );
+                }
+              )
+            ),
+            Positioned(
+              top: statusBarHeight,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BoardModule.provideBoardCreatePage(),
+                    )
+                  ).then((_) {
+                    final boardListProvider = Provider.of<BoardListProvider>(
+                      context, listen: false
+                    );
+                    
+                    boardListProvider.listBoard(1, 6);
+                  });
+                },
+                child: Icon(Icons.add),
+                tooltip: '게시물 생성',
+              )
+            ),
+          ],
+        )
+      )
     );
   }
 
