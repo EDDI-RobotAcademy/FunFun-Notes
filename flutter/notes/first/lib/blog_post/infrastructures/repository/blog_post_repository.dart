232e2556b50b5ecea3fa948a5ff857be6b797400
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-11 17:54:26 +0900

    feat: [이상훈] Flutter에서 s3 활용 준비 [SKAI8-x]

---
+++ flutter/notes/first/lib/blog_post/infrastructures/repository/blog_post_repository.dart
@@ -0,0 +1,11 @@
+import '../../domain/entity/blog_post.dart';
+import '../../domain/usecases/list/response/blog_post_list_response.dart';
+
+abstract class BlogPostRepository {
+  Future<BlogPostListResponse> requestList(int page, int perPage);
+  Future<BlogPost> create(String title, String content, String userToken);
+  Future<BlogPost?> readBlogPost(int id);
+  Future<BlogPost?> updateBlogPost(
+      int blogPostId, String title, String content, String userToken);
+  Future<void> deleteBlogPost(int id, String userToken);
+}
