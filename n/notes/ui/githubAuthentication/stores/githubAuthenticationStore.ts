d95e3a6e0b2ee7f7ad2d4eec00ea5cd5ea90b0e0
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-27 17:46:29 +0900

    feat: 통신 준비

---
+++ n/notes/ui/githubAuthentication/stores/githubAuthenticationStore.ts
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+import { githubAuthenticationState } from "./githubAuthenticationState";
+import { githubAuthenticationAction } from "./githubAuthenticationActions";
+
+export const useGithubAuthenticationStore = defineStore('githubAuthenticationStore', {
+    state: githubAuthenticationState,
+    actions: githubAuthenticationAction,
+})
