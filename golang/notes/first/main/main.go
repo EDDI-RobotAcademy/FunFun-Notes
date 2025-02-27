39ea76e4a1f6479ee4eb1f60cb96823e06c7709b
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-27 11:47:47 +0900

    feat: [이상훈] 포스트 작성 완료 [AI8-x]

---
+++ golang/notes/first/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"first/initializer"
+	"first/router"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// DB 초기화
+	db, err := initializer.DomainInitializer()
+	if err != nil {
+		fmt.Println("Error initializing domain:", err)
+		return
+	}
+
+	// Initialize the Fiber app
+	app := fiber.New()
+
+	// Register routes for all domains (posts, users, etc.)
+	router.RegisterRoutes(app, db)
+
+	// Start the server
+	if err := app.Listen(":3773"); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
