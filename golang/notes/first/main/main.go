6d2a06890a54cbced284b30705d0f723cf2b867c
Sanghoon Lee <gcccompil3r@gmail.com>
2025-02-27 12:02:52 +0900

    feat: [이상훈] 포트 처리 [AI8-x]

--- golang/notes/first/main/main.go
+++ golang/notes/first/main/main.go
@@ -4,11 +4,26 @@ import (
 	"first/initializer"
 	"first/router"
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	// 포트 번호를 .env에서 가져오기
+	port := os.Getenv("PORT")
+	if port == "" {
+		// 포트 번호가 없으면 에러 발생
+		fmt.Println("Error: PORT is not set in .env file")
+		return
+	}
+
 	// DB 초기화
 	db, err := initializer.DomainInitializer()
 	if err != nil {
